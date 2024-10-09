04b68c71557ea5a16c5010e30415ef5bc9e33ab1
jandomhof <j.h.domhof@outlook.com>
2024-10-09 11:40:48 +0200

    added stu-d image

--- src/pages/Locatie.js
+++ src/pages/Locatie.js
@@ -1,10 +1,25 @@
-import React, { useState } from "react";
-import { Stack, Box, Typography, Link } from "@mui/material";
+import React, { useState, useEffect, useRef } from "react";
+import { Stack, Box, Typography, Link, Grid } from "@mui/material";
 import PageTitle from "../components/PageTitle";
+import stud from "../images/Stu-D website.png";
 
 const Locatie = () => {
   const [width] = useState(window.innerWidth);
   const mobile = width < 768;
+  const imageRef = useRef(null);
+  const [iframeHeight, setIframeHeight] = useState(0);
+
+  useEffect(() => {
+    if (imageRef.current) {
+      setIframeHeight(imageRef.current.clientHeight);
+    }
+  }, [width]);
+
+  const handleImageLoad = () => {
+    if (imageRef.current) {
+      setIframeHeight(imageRef.current.clientHeight);
+    }
+  };
 
   return (
     <Stack
@@ -22,19 +37,38 @@ const Locatie = () => {
         marginBottom={10}
       />
 
-      {/* Google Maps iframe */}
-      <Box
-        component="iframe"
-        sx={{
-          border: 0,
-          width: mobile ? "100%" : "80%",
-          height: mobile ? "300px" : "450px",
-        }}
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d892.570024959601!2d4.347138347341277!3d52.015358865733205!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5b5334dcc0261%3A0xf7dd2ff3d2a6d624!2sStu-D%20Study%20Places!5e0!3m2!1sen!2snl!4v1728043020041!5m2!1sen!2snl"
-        allowFullScreen=""
-        loading="lazy"
-        title="Stu-D Location"
-      ></Box>
+      {/* Container for iframe and image */}
+      <Grid container spacing={2} justifyContent="center" width="100%">
+        {/* Image */}
+        <Grid item xs={12} md={6} marginLeft={mobile ? -2 : 0}>
+          <Box
+            component="img"
+            sx={{
+              width: "100%",
+            }}
+            src={stud}
+            alt="Stu-D Image"
+            ref={imageRef}
+            onLoad={handleImageLoad}
+          />
+        </Grid>
+
+        {/* Google Maps iframe */}
+        <Grid item xs={12} md={6} marginLeft={mobile ? -2 : 0}>
+          <Box
+            component="iframe"
+            sx={{
+              border: 0,
+              width: "100%",
+              height: `${iframeHeight}px`,
+            }}
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d892.570024959601!2d4.347138347341277!3d52.015358865733205!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5b5334dcc0261%3A0xf7dd2ff3d2a6d624!2sStu-D%20Study%20Places!5e0!3m2!1sen!2snl!4v1728043020041!5m2!1sen!2snl"
+            allowFullScreen=""
+            loading="lazy"
+            title="Stu-D Location"
+          ></Box>
+        </Grid>
+      </Grid>
 
       {/* Uitleg */}
       <Typography variant="body1" sx={{ maxWidth: 800 }}>
